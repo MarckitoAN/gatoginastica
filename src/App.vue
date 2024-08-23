da21558a0666e884e8cc55598da0fcd4694e23df
BrunoCampxs <bruno.andrade115@etec.sp.gov.br>
2024-08-23 17:21:49 -0300

    a

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Example paragraph="palavras"/>
+</template>
+
+<script>
+import Example from '.components/Example.vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld, Example
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Example.vue
@@ -0,0 +1,30 @@
+<template>
+
+    <div>
+        <p @click="handleClick">
+            Parágrafo: {{ paragraph }}
+        </p>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        props: ['paragraph'],
+        methods: {
+            handleClick() {
+                console.log('Clicar no parágrafo');
+            }
+        }
+    }
+
+</script>
+
+
+<style>
+    p{
+        background-color: pink;
+        font-weight: 400;
+        cursor: pointer;
+    }
+</style>
