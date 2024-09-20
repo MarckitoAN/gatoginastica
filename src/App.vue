5e29ac8cbd074240c95b21cc9b782c9b617d1826
BrunoCampxs <bruno.andrade115@etec.sp.gov.br>
2024-09-20 17:47:20 -0300

    ''

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-  <Example paragraph="palavras"/>
+  <ObjectComponent></ObjectComponent>
 </template>
 
 <script>
-import Example from '.components/Example.vue'
-import HelloWorld from './components/HelloWorld.vue'
-
+/*
+import LoopComponent from './components/LoopComponent.vue';
 export default {
   name: 'App',
   components: {
-    HelloWorld, Example
+    LoopComponent
   }
 }
+*/
+
+import ObjectComponent from './components/ObjectComponent.vue';
+export default {
+  name: 'App',
+  components:{
+    ObjectComponent
+  }
+}
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/LoopComponent.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+            <form @submit.prevent="addItem">  
+                <input type="text" v-model="inputValue">
+                <button @click="addItem" >Adicionar</button>
+            </form>
+            <table>
+                <tr>
+                    <td>index</td>
+                    <td>key</td>
+                </tr>
+                <tr v-for="(item, index) in itens" :key="index">
+                    <td>{{ index }}</td>
+                    <td> {{ item }}</td>
+                </tr>
+            </table>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return{ 
+            itens:[],
+            inputValue: ''
+        }
+    },
+    methods:{
+        addItem() {
+            if (this.inputValue) {
+                this.itens.push(this.inputValue)
+            }
+            this.inputValue = ''
+        }
+    }
+}
+</script>
+
+<style>
+</style>
